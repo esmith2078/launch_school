1d5346ad81bd98d652c75c34afda10331fecaa21
Eric Smith <esmith2078@gmail.com>
2020-12-19 15:48:01 -0500

    Updated RPS to RPSLS

--- RB101/lesson_2/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -1,6 +1,6 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICE = %w[rock paper scissors]
+VALID_CHOICE = %w[rock paper scissors lizard spock]
 
 def prompt(message)
   puts("=> #{message}")
@@ -9,7 +9,14 @@ end
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'lizard' && second == 'spock')
 end
 
 def display_results(player, computer)
@@ -22,12 +29,23 @@ def display_results(player, computer)
   end
 end
 
+def win_total(player, computer)
+  if win?(player, computer)
+    player_score =+ 1
+  elsif win?(computer, player)
+    computer_score =+ 1
+  end
+end
+
 prompt('Welcome to Roshambo!')
 loop do
   choice = ''
+  player_score = 0
+  computer_score = 0
   loop do
     puts
     prompt('Try your hand if you dare!')
+    puts
     prompt("Choose one: #{VALID_CHOICE.join(', ')}")
     choice = gets.chomp
 
@@ -41,10 +59,13 @@ loop do
   computer_choice = VALID_CHOICE.sample
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
   display_results(choice, computer_choice)
 
-  prompt('Do you wnat to play again? Y or N')
+  player_score = win_total(choice, computer_choice)
+  computer_score = win_total(choice, computer_choice)
+  #prompt("Player wins: #{player_score}; Computer wins: #{computer_score}")
+
+  prompt('Do you want to play again? Y or N')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
